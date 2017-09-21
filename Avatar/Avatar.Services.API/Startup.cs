3f16bf0b84d4fa23c901672f56543542f166993b
Phellipe Matos <cphellipematos@outlook.com>
2017-09-21 13:18:03 -0300

    Set up project swagger API documentation

--- Avatar/Avatar.Services.API/Startup.cs
+++ Avatar/Avatar.Services.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Avatar.Services.API
 {
@@ -24,7 +25,12 @@ namespace Avatar.Services.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -33,6 +39,12 @@ namespace Avatar.Services.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
